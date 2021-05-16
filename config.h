09fed365682ebecbd01a8a8bdd543a2de48deaa7
Ivana <ivana.huckova@gmail.com>
2021-05-16 17:44:36 +0200

    Initial commit

---
+++ config.h
@@ -0,0 +1,10 @@
+
+// WiFi
+#define WIFI_SSID     "wifi_name" // Add wifi name
+#define WIFI_PASSWORD "wifi_password" // Add wifi passowrd
+
+#define ID "weather" // Add unique name for this sensor
+#define INTERVAL 10 //seconds
+
+#define DHTPIN 4    // Which pin is DHT 11 connected to
+#define DHTTYPE DHT11 // Type DHT 11
