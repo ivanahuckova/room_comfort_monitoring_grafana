9393d0cd9a09192a1f0a3640d1b0a55f81cc739b
Ivana <ivana.huckova@gmail.com>
2021-05-23 16:59:22 +0200

    Use Prometheus client

--- config.h
+++ config.h
@@ -1,5 +1,3 @@
-
-// WiFi
 #define WIFI_SSID     "wifi_name" // Add wifi name
 #define WIFI_PASSWORD "wifi_password" // Add wifi passowrd
 
@@ -8,3 +6,9 @@
 
 #define DHTPIN 4    // Which pin is DHT 11 connected to
 #define DHTTYPE DHT11 // Type DHT 11
+
+#define GC_URL "prometheus-us-central1.grafana.net" // Url to Prometheus instance 
+#define GC_PATH "/api/prom/push" // Path
+#define GC_PORT 443
+#define GC_USER "" // Username
+#define GC_PASS "" // API key
--- weather_monitoring_grafana.ino
+++ weather_monitoring_grafana.ino
@@ -1,35 +1,52 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <NTPClient.h>
-
+#include <Prometheus.h>
+#include <bearssl_x509.h>
 #include <DHT.h>
 
+#include "certificates.h"
 #include "config.h"
-
-// NTP Client
-WiFiUDP ntpUDP;
-NTPClient ntpClient(ntpUDP);
 
 // DHT Sensor
 DHT dht(DHTPIN, DHTTYPE);
 
-// Function to set up the connection to the WiFi
-void setupWiFi() {
-  Serial.print("Connecting to '");
-  Serial.print(WIFI_SSID);
-  Serial.print("' ...");
+// Prometheus client
+Prometheus client;
+// Create a write request for 3 series
+WriteRequest req(3, 768);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+// Create a labelset arrays for the 2 labels that is going to be used for all series
+LabelSet label_set[] = {{ "monitoring_type", "weather" }, { "board_type", "esp32-devkit1" }};
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+// Define a TimeSeries which can hold up to 5 samples, has a name of `temperature/humidity/...` and uses the above labels of which there are 2
+TimeSeries ts1(5, "temperature_celsius", label_set, 2);
+TimeSeries ts2(5, "humidity_percent", label_set, 2);
+TimeSeries ts3(5, "heat_index_celsius", label_set, 2);
+
+int loopCounter = 0;
+
+// Function to set up Prometheus client
+void setupClient() {
+  Serial.println("Setting up client...");
+  // Configure the client
+  client.setUrl(GC_URL);
+  client.setPath(GC_PATH);
+  client.setPort(GC_PORT);
+  client.setUser(GC_USER);
+  client.setPass(GC_PASS);
+  client.setUseTls(true);
+  client.setCerts(TAs, TAs_NUM);
+  client.setWifiSsid(WIFI_SSID);
+  client.setWifiPass(WIFI_PASSWORD);
+  client.setDebug(Serial);  // Remove this line to disable debug logging of the client.
+  if (!client.begin()){
+      Serial.println(client.errmsg);
   }
 
-  Serial.println("connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  // Add our TimeSeries to the WriteRequest
+  req.addTimeSeries(ts1);
+  req.addTimeSeries(ts2);
+  req.addTimeSeries(ts3);
+  req.setDebug(Serial);  // Remove this line to disable debug logging of the write request serialization and compression.
 }
 
 
@@ -39,11 +56,8 @@ void setup() {
   // Start the serial output at 115,200 baud
   Serial.begin(115200);
 
-  // Connect to WiFi
-  setupWiFi();
-
-  // Initialize a NTPClient to get time
-  ntpClient.begin();
+  // Set up client
+  setupClient();
 
   // Start the DHT sensor
   dht.begin();
@@ -52,23 +66,10 @@ void setup() {
 
 // LOOP: Function called in a loop to read from sensors and send them do databases
 void loop() {
-  // Reconnect to WiFi if required
-  if (WiFi.status() != WL_CONNECTED) {
-    WiFi.disconnect();
-    yield();
-    setupWiFi();
-  }
+  int64_t time;
+  time = client.getTimeMillis();
 
-  // Update time via NTP if required
-  while(!ntpClient.update()) {
-    yield();
-    ntpClient.forceUpdate();
-  }
-
-  // Get current timestamp
-  unsigned long ts = ntpClient.getEpochTime();
-
-  // Read temperature, humidity and distance
+  // Read temperature and humidity
   float hum = dht.readHumidity();
   float cels = dht.readTemperature();
   
@@ -81,11 +82,29 @@ void loop() {
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(cels, hum, false);
 
-  Serial.println(ts);
-  Serial.println(cels);
-  Serial.println("==============");
-
-  // wait INTERVAL seeconds and then do it again
+  if (loopCounter >= 5) {
+    //Send
+    loopCounter = 0;
+    if (!client.send(req)) {
+      Serial.println(client.errmsg);
+    }
+    // Reset batches after a succesful send.
+    ts1.resetSamples();
+    ts2.resetSamples();
+    ts3.resetSamples();
+  } else {
+    if (!ts1.addSample(time, cels)) {
+      Serial.println(ts1.errmsg);
+    }
+    if (!ts2.addSample(time, hum)) {
+      Serial.println(ts2.errmsg);
+    }
+    if (!ts3.addSample(time, hic)) {
+      Serial.println(ts3.errmsg);
+    }
+    loopCounter++;
+  }
+  // wait INTERVAL seconds and then do it again
   delay(INTERVAL * 1000);
 }
 
